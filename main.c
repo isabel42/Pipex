6bf04eaccae5d1360e9d1502eb5edc6147d307e6
isabel42 <106957731+isabel42@users.noreply.github.com>
2023-03-13 16:52:27 +0100

    flags

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 19:49:04 by itovar-n          #+#    #+#             */
-/*   Updated: 2023/03/10 16:11:50 by itovar-n         ###   ########.fr       */
+/*   Updated: 2023/03/13 16:51:26 by itovar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@
 #include <fcntl.h>
 #include "libft/libft.h"
 
+char	*ft_path(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+			return (envp[i]);
+		i++;
+	}
+	return (NULL);
+}
+
 char	*ft_find_path(char *path, char *command)
 {
 	char	**path_split;
@@ -23,13 +37,13 @@ char	*ft_find_path(char *path, char *command)
 	int		i;
 
 	i = 0;
-	command = ft_strjoin("/",command);
+	command = ft_strjoin("/", command);
 	path_split = ft_split(path, ':');
 	while (path_split[i])
 	{
 		path_command = ft_strjoin(path_split[i], command);
 		if (access(path_command, X_OK) == 0)
-			break;
+			break ;
 		free(path_command);
 		i++;
 	}
@@ -44,40 +58,80 @@ char	*ft_find_path(char *path, char *command)
 	return (path_command);
 }
 
-
-int main(int argc, char **argv, char **envp)
+char	*ft_command (char **argv, int i)
 {
+	char	**command_flags;
+	char	*command;
+	int		j;
+
+	j = 0;
+	command_flags = ft_split(argv[i + 1], ' ');
+	command = command_flags[0];
+	while (command_flags[j])
+	{
+		free(command_flags[j]);
+		j++;
+	}
+	free(command_flags);
+	return(command);
+}
+
+char	*ft_flags (char **argv, int i)
+{
+	char	**command_flags;
+	char	*command;
+	char	*flags;
+	int		j;
+
+	j = 0;
+	command_flags = ft_split(argv[i + 1], ' ');
+	command = command_flags[0];
+	flags = ft_strtrim(argv[i + 1], command);
+	flags = ft_strtrim(flags, " ");
+	while (command_flags[j])
+	{
+		free(command_flags[j]);
+		j++;
+	}
+	free(command_flags);
+	printf("flags:%s\n", flags);
+	return(flags);
+}
+
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*flags1[] = {"pipex", "-la", NULL};
+	char	*flags2[] = {"pipex", "-l", NULL};
+	int		p1[2];
+
 	if (argc == 10)
-		return(10);
-	char *flags1[] = {"pipex", "-la", NULL};
-	// char *flags1[] = {"ping", "-c", "5", "google.com", NULL};
-	char *flags2[] = {"pipex", "-l", NULL};
-	int p1[2]; // c -> p
-	if(pipe(p1) == -1)
-		return 1; 
-	int pid1 = fork();
+		return (10);
+	if (pipe(p1) == -1)
+		return 1;
+	int	pid1 = fork();
 	if (pid1 < 0)
-		return 2;
+		return (2);
 	if (pid1 == 0)
 	{
 		dup2(p1[1], STDOUT_FILENO);
 		close(p1[0]);
 		close(p1[1]);
-		execve(ft_find_path(envp [5], argv[1]), flags1, NULL);
+		execve(ft_find_path(ft_path(envp), argv[1]), flags1, NULL);
 	}
 	int pid2 = fork();
 	if (pid2 < 0)
-		return 2;
+		return (2);
 	if (pid2 == 0)
 	{
 		dup2(p1[0], STDIN_FILENO);
 		close(p1[0]);
 		close(p1[1]);
-		execve(ft_find_path(envp [5], argv[2]), flags2, NULL);
+		execve(ft_find_path(ft_path(envp), argv[2]), flags2, NULL);
 	}
 	close(p1[0]);
 	close(p1[1]);
 	waitpid(pid1, NULL, 0);
 	waitpid(pid2, NULL, 0);
-	return 0;
+	return (0);
 } 
